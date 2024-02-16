16454f9b40945d917b9d1d8162d655e4bcac525b
Celeste Francier <cfrancie@e2r4p19.42.fr>
2024-02-16 18:15:26 +0100

    feat: basic implementing of sockets

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,10 +1,37 @@
 #pragma once
 
 #include <iostream>
+#include <netinet/in.h> // sockaddr_in
+#include <sys/socket.h> // socket
+#include <unistd.h>     // close
 
-class Test {
+#define PORT 8080
+
+class Request {
+private:
+  int server_fd;
+  int new_socket;
+  struct sockaddr_in client_addr;
+
 public:
-    void print() {
-        std::cout << "Hello, World!" << std::endl;
-    };
+  Request();
+  ~Request();
+  int init_socket(int port);
 };
+
+class Response {
+
+};
+
+class CGI {
+
+};
+
+class Server {
+private:
+  Request request;
+  Response response;
+  CGI cgi;
+};
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,61 @@
-#include "main.hpp"
+#include "../inc/main.hpp"
 
-int main() {
-    Test test;
-    test.print();
+static int init_socket(int port) {
+    int server_fd;
+    struct sockaddr_in server_addr;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cerr << "socket failed" << std::endl;
+        return -1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
+            0) {
+        std::cerr << "bind failed" << std::endl;
+        return -1;
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        std::cerr << "listen failed" << std::endl;
+        return -1;
+    }
+
+    return server_fd;
+}
+
+int main(void) {
+    int server_fd, new_socket;
+    struct sockaddr_in client_addr;
+
+    if ((server_fd = init_socket(PORT)) < 0) {
+        return -1;
+    }
+
+    int addrlen = sizeof(client_addr);
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr,
+                             (socklen_t *)&addrlen)) < 0) {
+        std::cerr << "accept failed" << std::endl;
+        return -1;
+    }
+
+    char buffer[1024] = {0};
+
+    int valread;
+
+    if ((valread = read(new_socket, buffer, 1024)) < 0) {
+        std::cerr << "read failed" << std::endl;
+        return -1;
+    }
+
+    std::cout << buffer << std::endl;
+    send(new_socket, "Hello from server", 18, 0);
+
+    close(new_socket);
+    close(server_fd);
+
     return 0;
 }
