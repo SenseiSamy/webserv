411243669303d58802013073d657899aa07ce9e4
wmemcpy <celeste.francier@protonmail.com>
2024-02-20 16:29:16 +0100

    Add .clang-format, .gitignore, and update main.cpp***

--- inc/main.hpp
+++ inc/main.hpp
@@ -1,37 +1,73 @@
 #pragma once
 
-#include <iostream>
-#include <netinet/in.h> // sockaddr_in
+#include <cerrno>       // errno
+#include <cstdlib>      // exit
+#include <cstring>      // strlen, memset
+#include <iostream>     // std::cerr, std::cout
+#include <netinet/in.h> // sockaddr_in, htons/htonl
 #include <sys/socket.h> // socket
-#include <unistd.h>     // close
+#include <sys/types.h>  // socket
+#include <unistd.h>     // close, write
+#include <strings.h>    // bzero
 
-#define PORT 8080
+#define RESET "\033[0m"
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
 
-class Request {
-private:
-  int server_fd;
-  int new_socket;
-  struct sockaddr_in client_addr;
+/*
+class Request
+{
+  private:
+    int server_fd;
+    int new_socket;
+    struct sockaddr_in client_addr;
 
-public:
-  Request();
-  ~Request();
-  int init_socket(int port);
+  public:
+    Request() : server_fd(-1), new_socket(-1)
+    {
+    }
+    Request(int server_fd) : server_fd(server_fd), new_socket(-1)
+    {
+    }
+    Request(const Request &other)
+    {
+        *this = other;
+    }
+    ~Request()
+    {
+        close(server_fd);
+    }
+
+    Request &operator=(const Request &other)
+    {
+        server_fd = other.server_fd;
+        new_socket = other.new_socket;
+        client_addr = other.client_addr;
+        return *this;
+    }
+
+    int init_socket(int port);
 };
 
-class Response {
-
+class Response
+{
 };
 
-class CGI {
-
+class CGI
+{
 };
 
-class Server {
-private:
-  Request request;
-  Response response;
-  CGI cgi;
+class Server
+{
+  private:
+    Request request;
+    Response response;
+    CGI cgi;
 };
-
-
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,61 @@
-#include "../inc/main.hpp"
+#include "main.hpp"
 
-static int init_socket(int port) {
-    int server_fd;
-    struct sockaddr_in server_addr;
+const int port = 8080;
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        std::cerr << "socket failed" << std::endl;
-        return -1;
+void handle_client(int client_sock)
+{
+    const char *http_response = "HTTP/1.1 200 OK\r\n"
+                                "Content-Type: text/plain\r\n"
+                                "Content-Length: 12\r\n"
+                                "\r\n"
+                                "Hello World!";
+
+    write(client_sock, http_response, strlen(http_response));
+}
+
+int main(void)
+{
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t sin_len = sizeof(client_addr);
+
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock < 0)
+    {
+        std::cerr << "socket error: " << strerror(errno) << std::endl;
+        exit(1);
     }
+
+    int optval = 1;
+    setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
+    bzero(&server_addr.sin_zero, sizeof(server_addr.sin_zero));
 
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
-            0) {
-        std::cerr << "bind failed" << std::endl;
-        return -1;
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        std::cerr << "bind error: " << strerror(errno) << std::endl;
+        exit(2);
     }
 
-    if (listen(server_fd, 3) < 0) {
-        std::cerr << "listen failed" << std::endl;
-        return -1;
+    listen(server_sock, 5);
+    std::cout << "Server is running on port " << port << std::endl;
+
+    while (true)
+    {
+        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &sin_len);
+        if (client_sock < 0)
+        {
+            std::cerr << "accept error: " << strerror(errno) << std::endl;
+            continue;
+        }
+
+        handle_client(client_sock);
+        close(client_sock);
     }
 
-    return server_fd;
-}
-
-int main(void) {
-    int server_fd, new_socket;
-    struct sockaddr_in client_addr;
-
-    if ((server_fd = init_socket(PORT)) < 0) {
-        return -1;
-    }
-
-    int addrlen = sizeof(client_addr);
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr,
-                             (socklen_t *)&addrlen)) < 0) {
-        std::cerr << "accept failed" << std::endl;
-        return -1;
-    }
-
-    char buffer[1024] = {0};
-
-    int valread;
-
-    if ((valread = read(new_socket, buffer, 1024)) < 0) {
-        std::cerr << "read failed" << std::endl;
-        return -1;
-    }
-
-    std::cout << buffer << std::endl;
-    send(new_socket, "Hello from server", 18, 0);
-
-    close(new_socket);
-    close(server_fd);
-
+    close(server_sock);
     return 0;
 }
