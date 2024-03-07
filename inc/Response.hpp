ea96749f76f1ec2749daa102b500a6debef31870
wmari <wmari@student.42.fr>
2024-03-07 14:46:45 +0100

    feat : creation de page automatique pour error, WIP

--- inc/Response.hpp
+++ inc/Response.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <map>
 
 #define GET 1
 #define POST 2
@@ -19,64 +20,120 @@ enum type
     UNKNOW
 };
 
+class HttpErrorCodes {
+private:
+    typedef std::map<int, std::string> ErrorCodeMap;
+    ErrorCodeMap httpErrorCodes;
+
+public:
+    // Default constructor
+    HttpErrorCodes() {
+        initializeErrorCodes();
+    }
+
+    // Copy constructor
+    HttpErrorCodes(const HttpErrorCodes& other) {
+        httpErrorCodes = other.httpErrorCodes;
+    }
+
+    // Copy assignment operator
+    HttpErrorCodes& operator=(const HttpErrorCodes& other) {
+        if (this != &other) {
+            httpErrorCodes = other.httpErrorCodes;
+        }
+        return *this;
+    }
+
+    // Destructor
+    ~HttpErrorCodes() {}
+
+    std::string getDescription(int errorCode) const {
+        ErrorCodeMap::const_iterator it = httpErrorCodes.find(errorCode);
+        if (it != httpErrorCodes.end()) {
+            return it->second;
+        }
+        return "Unknown Error";
+    }
+
+private:
+    // Initialize error codes
+    void initializeErrorCodes() {
+        httpErrorCodes[100] = "Continue";
+        httpErrorCodes[101] = "Switching Protocols";
+        httpErrorCodes[102] = "Processing";
+        httpErrorCodes[200] = "OK";
+        httpErrorCodes[201] = "Created";
+        httpErrorCodes[202] = "Accepted";
+        httpErrorCodes[203] = "Non-Authoritative Information";
+        httpErrorCodes[204] = "No Content";
+        httpErrorCodes[205] = "Reset Content";
+        httpErrorCodes[206] = "Partial Content";
+        httpErrorCodes[207] = "Multi-Status";
+        httpErrorCodes[208] = "Already Reported";
+        httpErrorCodes[226] = "IM Used";
+        httpErrorCodes[300] = "Multiple Choices";
+        httpErrorCodes[301] = "Moved Permanently";
+        httpErrorCodes[302] = "Found";
+        httpErrorCodes[303] = "See Other";
+        httpErrorCodes[304] = "Not Modified";
+        httpErrorCodes[305] = "Use Proxy";
+        httpErrorCodes[306] = "(Unused)";
+        httpErrorCodes[307] = "Temporary Redirect";
+        httpErrorCodes[308] = "Permanent Redirect";
+        httpErrorCodes[400] = "Bad Request";
+        httpErrorCodes[401] = "Unauthorized";
+        httpErrorCodes[402] = "Payment Required";
+        httpErrorCodes[403] = "Forbidden";
+        httpErrorCodes[404] = "Not Found";
+        httpErrorCodes[405] = "Method Not Allowed";
+        httpErrorCodes[406] = "Not Acceptable";
+        httpErrorCodes[407] = "Proxy Authentication Required";
+        httpErrorCodes[408] = "Request Timeout";
+        httpErrorCodes[409] = "Conflict";
+        httpErrorCodes[410] = "Gone";
+        httpErrorCodes[411] = "Length Required";
+        httpErrorCodes[412] = "Precondition Failed";
+        httpErrorCodes[413] = "Payload Too Large";
+        httpErrorCodes[414] = "URI Too Long";
+        httpErrorCodes[415] = "Unsupported Media Type";
+        httpErrorCodes[416] = "Range Not Satisfiable";
+        httpErrorCodes[417] = "Expectation Failed";
+        httpErrorCodes[418] = "I'm a teapot";
+        httpErrorCodes[421] = "Misdirected Request";
+        httpErrorCodes[422] = "Unprocessable Entity";
+        httpErrorCodes[423] = "Locked";
+        httpErrorCodes[424] = "Failed Dependency";
+        httpErrorCodes[425] = "Too Early";
+        httpErrorCodes[426] = "Upgrade Required";
+        httpErrorCodes[428] = "Precondition Required";
+        httpErrorCodes[429] = "Too Many Requests";
+        httpErrorCodes[431] = "Request Header Fields Too Large";
+        httpErrorCodes[451] = "Unavailable For Legal Reasons";
+        httpErrorCodes[500] = "Internal Server Error";
+        httpErrorCodes[501] = "Not Implemented";
+        httpErrorCodes[502] = "Bad Gateway";
+        httpErrorCodes[503] = "Service Unavailable";
+        httpErrorCodes[504] = "Gateway Timeout";
+        httpErrorCodes[505] = "HTTP Version Not Supported";
+        httpErrorCodes[506] = "Variant Also Negotiates";
+        httpErrorCodes[507] = "Insufficient Storage";
+        httpErrorCodes[508] = "Loop Detected";
+        httpErrorCodes[510] = "Not Extended";
+        httpErrorCodes[511] = "Network Authentication Required";
+    }
+};
+
 class Response
 {
   private:
     int client_sock;
     std::string request;
     int type;
+    HttpErrorCodes hec;
 
     void logError(const std::string &message)
     {
         std::cerr << "Error: " << message << std::endl;
-    }
-
-  public:
-    // Constructor
-    Response(int sock, const char *req)
-    {
-        client_sock = sock;
-        request = req;
-    }
-
-    // Destructor
-    ~Response()
-    {
-        close(client_sock);
-    }
-
-    // Copy constructor
-    Response(const Response &other)
-    {
-        client_sock = other.client_sock;
-        request = other.request;
-    }
-
-    // Assignment operator
-    Response &operator=(const Response &other)
-    {
-        if (this != &other)
-        {
-            client_sock = other.client_sock;
-            request = other.request;
-        }
-        return *this;
-    }
-
-    void sendResponse()
-    {
-        std::string filename = extractFilename();
-        std::ifstream file(filename.c_str());
-        if (!file.is_open())
-            log(ERROR, "filename:" + std::string(strerror(errno)));
-        std::string response;
-        std::string line;
-        while (std::getline(file, line))
-            response += line + "\n";
-        file.close();
-
-        std::string httpResponse = generateHTTPResponse(response);
-        send(client_sock, httpResponse.c_str(), httpResponse.size(), 0);
     }
 
     std::string extractFilename()
@@ -120,5 +177,82 @@ class Response
                 httpResponse = "HTTP/1.1 200 OK\n\n" + content; // default to plain text
         }
         return httpResponse;
+    }
+
+
+
+  public:
+    // Constructor
+    Response(int sock, const char *req)
+    {
+        client_sock = sock;
+        request = req;
+    }
+
+    // Destructor
+    ~Response()
+    {
+        close(client_sock);
+    }
+
+    // Copy constructor
+    Response(const Response &other)
+    {
+        client_sock = other.client_sock;
+        request = other.request;
+    }
+
+    // Assignment operator
+    Response &operator=(const Response &other)
+    {
+        if (this != &other)
+        {
+            client_sock = other.client_sock;
+            request = other.request;
+        }
+        return *this;
+    }
+
+    void sendResponse()
+    {
+        std::string filename = extractFilename();
+        std::ifstream file(filename.c_str());
+        if (!file.is_open())
+        {
+            log(ERROR, "filename:" + std::string(strerror(errno)));
+            generateHTTPError(404);
+        }
+        else
+        {
+            std::string response;
+            std::string line;
+            while (std::getline(file, line))
+                response += line + "\n";
+            file.close();
+
+            std::string httpResponse = generateHTTPResponse(response);
+            send(client_sock, httpResponse.c_str(), httpResponse.size(), 0);
+        }
+    }
+
+    void    generateHTTPError(int num)
+    {
+        std::string reponse;
+        std::ifstream file("www/ErrorPage");
+        std::string line;
+        reponse += "HTTP/1.1 ";
+        reponse += "404";
+        reponse += " ";
+        reponse += hec.getDescription(num);
+        reponse += "\n\r\n\r";
+        while(std::getline(file,line))
+            reponse += line + "\n\r";
+        file.close();
+        reponse += "<img src=\"https://http.cat/";
+        reponse += "404\"\n\rwidth=\"400\"\nheight=\"341\"/>\n\r";
+        reponse += "</body>\n\r</html>\n\r";
+        std::cout << reponse << std::endl;
+
+        send(client_sock, reponse.c_str(), reponse.size(), 0);
     }
 };
