11b169b16eac83a805779f07082d6c7a894c8c2a
wmari <wmari@student.42.fr>
2024-05-15 20:33:11 +0200

    trouvé

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -107,4 +107,47 @@ void Request::parse()
 			_body.assign(std::istreambuf_iterator<char>(iss), std::istreambuf_iterator<char>()); // Read the rest of the stream
 		}
 	}
+	// std::istringstream stream(_request);
+    // std::string requestLine;
+    // if (!std::getline(stream, requestLine))
+    //     return;
+
+    // // Extract request line
+    // std::istringstream requestLineStream(requestLine);
+    // if (!std::getline(requestLineStream, _method, ' ') || !std::getline(requestLineStream, _url, ' '))
+    //     return;
+
+    // // Extract headers lines
+    // std::string line;
+    // while (std::getline(stream, line))
+    // {
+    //     std::string headername;
+    //     std::string headervalue;
+    //     size_t colonPos = line.find_first_of(':');
+    //     if (line.empty() || colonPos == std::string::npos)
+    //         break;
+    //     else
+    //     {
+    //         headername = line.substr(0, colonPos);
+    //         headervalue = line.substr(colonPos + 1);
+    //         headervalue.erase(headervalue.size() - 1);
+    //         while (headername.find('\t') != std::string::npos)
+    //             headername = headername.substr(headername.find('\t'));
+    //         _headers[headername] = headervalue;
+    //     }
+    // }
+    // if (!_method.compare("POST"))
+    // {
+    //     std::stringstream temp;
+	// 	temp << _headers["Content-Length"];
+	// 	temp >> _content_lenght;
+    //     _body += "\r\n\r\n";
+	// 	size_t	count = 0;
+	// 	char buff[5000];
+	// 	while (count < _content_lenght && !stream.eof())
+	// 	{
+	// 		stream.read(buff, 5000);
+	// 		_body.append(buff, stream.gcount());
+	// 	}
+    // }
 }
