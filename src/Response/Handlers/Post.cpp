47a575913dec30d204240e2525c13ea1aa2bbdce
SenseiSamy <samy.naji2003@gmail.com>
2024-05-30 14:32:43 +0100

    forms from multipart/form-data are ignored

--- src/Response/Handlers/Post.cpp
+++ src/Response/Handlers/Post.cpp
@@ -56,6 +56,32 @@ static ssize_t multipart_file(std::string filename, std::ifstream& body, std::st
 	}
 }
 
+static ssize_t multipart_form(std::ifstream& body, std::string boundary) {
+	char buffer[BUFFER_SIZE + 1];
+	std::size_t	match_counter = 0;
+	std::size_t bytes_read = 0;
+
+	while (true) {
+		body.read(buffer, BUFFER_SIZE);
+		if (body.bad())
+			return (-1);
+		for (ssize_t i = 0; i < body.gcount(); ++i) {
+			if (buffer[i] == boundary[match_counter]) {
+				++match_counter;
+				if (match_counter == boundary.size())
+					return (bytes_read);
+			}
+			else {
+				if (match_counter > 0) {
+					bytes_read += match_counter;
+					match_counter = 0;
+				}
+				++bytes_read;
+			}
+		}
+	}
+}
+
 void Response::_multipart() 
 {
 	std::string line;
@@ -88,25 +114,25 @@ void Response::_multipart()
 					filename = line.substr(pos + 9, std::string::npos);
 					filename.erase(std::remove(filename.begin(), filename.end(), '"'), filename.end());
 				}
-				//else
-				// faire qlq chose quand c'est pas un fichier
 			}
 		}
 		if (!body.good()) {
 			_generate_response_code(400);
 			return;
 		}
-		if (!filename.empty()) {
-			std::streampos begin = body.tellg();
-			ssize_t i = multipart_file(filename, body, boundary);
-			if (i == -1) {
-				_generate_response_code(400);
-				return;
-			}
-			body.clear();
-			body.seekg(begin);
-			body.seekg(i, std::ios_base::cur);
+		std::streampos begin = body.tellg();
+		ssize_t i;
+		if (!filename.empty())
+			i = multipart_file(filename, body, boundary);
+		else
+			i = multipart_form(body, boundary);
+		if (i == -1) {
+			_generate_response_code(400);
+			return;
 		}
+		body.clear();
+		body.seekg(begin);
+		body.seekg(i, std::ios_base::cur);
 		crlf_getline(body, line);
 	}
 	_generate_response_code(201);
@@ -128,58 +154,3 @@ void Response::_post()
 	else
 		_generate_response_code(400);
 }
-
-/*void Response::_post()
-{
-	std::string boundaries = _request.get_headers_key("Content-Type");
-	size_t boundpos = boundaries.find("boundary=");
-	if (boundpos != std::string::npos)
-	{
-		boundaries = boundaries.substr(boundpos + 9);
-		// std::cout << boundaries << std::endl;
-		std::istringstream data(this->_request.get_body());
-		std::string filename;
-		std::string line;
-		size_t bodyread = 0;
-		int tokenheader = 2;
-		while (std::getline(data, line) && tokenheader)
-		{
-			if (line.find(boundaries))
-				bodyread += line.length();
-			if (line.find("filename=") != std::string::npos)
-			{
-				filename = line.substr(line.find("filename=") + 10);
-				tokenheader--;
-				bodyread += line.length();
-			}
-			else if (line.find("Content-Type:") != std::string::npos)
-			{
-				tokenheader--;
-				bodyread += line.length();
-			}
-			if (tokenheader == 0)
-				break;
-		}
-		filename = filename.substr(0, filename.size() - 2);
-
-		std::cout << boundaries << std::endl;
-		std::getline(data, line);
-		std::ofstream file(filename.c_str(), std::ios::binary);
-		char buff[5000];
-		size_t contentadd = 1;
-
-		while (5000 * contentadd < _request.get_content_length())
-		{
-			data.read(buff, 5000);
-			file.write(buff, 5000);
-			contentadd++;
-		}
-		data.read(buff, _request.get_content_length() -
-							(5000 * (contentadd - 1) + (bodyread + boundaries.length() + 10)));
-		file.write(buff, _request.get_content_length() -
-								(5000 * (contentadd - 1) + (bodyread + boundaries.length() + 10)));
-		file.close();
-
-		
-	}
-}*/
